790389009b6ae9f5aa00c85cfcbdb2e11b05f72e
Gramding <skyfilexd@Googlemail.com>
2025-05-27 20:38:28 +0200

    part 6 done

--- Code/components/fighter.py
+++ Code/components/fighter.py
@@ -1,4 +1,12 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 from components.base_component import BaseComponent
+from input_handlers import GameOverEventHandler
+from render_order import RenderOrder
+
+if TYPE_CHECKING:
+    from entity import Actor
 
 
 class Fighter(BaseComponent):
@@ -17,3 +25,21 @@ class Fighter(BaseComponent):
         # this enshures that HP can never be negative
         # and never more than maximum
         self._hp = max(0, min(value, self.max_hp))
+        if self._hp == 0 and self.entity.ai:
+            self.die()
+
+    def die(self) -> None:
+        if self.engine.player is self.entity:
+            death_message = "You died!"
+            self.engine.event_handler = GameOverEventHandler(self.engine)
+        else:
+            death_message = f"{self.entity.name} is dead!"
+
+        self.entity.char = "%"
+        self.entity.color = (191, 0, 0)
+        self.entity.blocks_movement = False
+        self.entity.ai = None
+        self.entity.name = f"remains of {self.entity.name}"
+        self.entity.render_order = RenderOrder.CORPSE
+
+        print(death_message)
