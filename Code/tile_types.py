6b38df90c431f4693f0da15cdfb8717b0f95f624
Gramding <skyfilexd@Googlemail.com>
2025-05-29 16:12:19 +0200

    color changes

--- Code/tile_types.py
+++ Code/tile_types.py
@@ -49,12 +49,12 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
     walkable=True,
     transparent=True,
-    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
-    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
+    dark=(ord(" "), (255, 255, 255), (43, 51, 49)),
+    light=(ord(" "), (255, 255, 255), (61, 18, 3)),
 )
 wall = new_tile(
     walkable=False,
     transparent=False,
-    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
-    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
+    dark=(ord(" "), (255, 255, 255), (50, 92, 82)),
+    light=(ord(" "), (255, 255, 255), (92, 26, 5)),
 )
