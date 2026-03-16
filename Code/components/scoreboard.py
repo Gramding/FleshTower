9b869754d4ecf95b0f04b68d5e62673ef2363056
Gramding <skyfilexd@googlemail.com>
2026-03-16 15:11:37 +0100

    implemented max 10 Scores in highscore list. If New Score is relevant it replaces an old one wich is hopefully the smallest one in the list :D

--- Code/components/scoreboard.py
+++ Code/components/scoreboard.py
@@ -10,7 +10,30 @@ class Scoreboard:
         pass
 
     def add_score(self, new_score):
-        self.scores.update({datetime.now(): new_score})
+        if self.is_score_relevant(new_score=new_score):
+            self.scores.update({datetime.now(): new_score})
+
+    def is_score_relevant(self, new_score) -> bool:
+        score_to_replace = 0
+        for score in self.scores:
+            if new_score >= self.scores[score] and self.scores[score] >= score_to_replace:
+                score_to_replace = score
+        if len(self.scores) < 1:
+            return True
+        try:
+            if self.scores[score_to_replace] > 0 and len(self.scores) >= 10:
+                self.scores.pop(score_to_replace)
+                return True
+        except KeyError:
+            if len(self.scores) <= 10:
+                return True
+            else:
+                return False
+
+        
+
+        
+
 
     def save(self):
         save_data = lzma.compress(pickle.dumps(self))
